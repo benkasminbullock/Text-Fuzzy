f07be9bf915b779fb75667435d06223074affd4d
Ben Bullock <benkasminbullock@gmail.com>
2012-07-26 08:40:45 +0900

    Add spell checker example

---
+++ examples/spell-check.pl
@@ -0,0 +1,66 @@
+#!/home/ben/software/install/bin/perl
+use warnings;
+use strict;
+use Getopt::Long;
+use Text::Fuzzy;
+use Lingua::EN::PluralToSingular 'to_singular';
+
+my $dict = '/usr/share/dict/words';
+GetOptions (
+    "dict=s" => \$dict,
+);
+my @words;
+my %words;
+my $min_length = 4;
+read_dictionary ($dict, \@words, \%words);
+# Known mistakes, don't repeat.
+my %known;
+# Spell-check each file on the command line.
+for my $file (@ARGV) {
+    open my $input, "<", $file or die "Can't open $file: $!";
+    while (<$input>) {
+        my @line = split /[^a-z]+/i, $_;
+        for my $word (@line) {
+            my $clean_word = to_singular (lc $word);
+            if ($words{$clean_word}) {
+                # It is in the dictionary.
+                next;
+            }
+            if (length $word < $min_length) {
+                next;
+            }
+            if ($word eq uc $word) {
+                next;
+            }
+            if ($known{$clean_word}) {
+                next;
+            }
+            my $tf = Text::Fuzzy->new ($clean_word);
+            my $nearest = $tf->nearest (\@words);
+            if (defined $nearest) {
+                my $correction = $words[$nearest];
+                print "$file:$.: '$word' may be $correction.\n";
+                $known{$clean_word} = $correction;
+            }
+            else {
+                print "$file:$.: $word may be a spelling mistake.\n";
+                $known{$clean_word} = 1;
+            }
+        }
+    }
+    close $input or die $!;
+}
+
+exit;
+
+sub read_dictionary
+{
+    my ($dict, $words_array, $words_hash) = @_;    
+    open my $din, "<", $dict or die "Can't open dictionary $dict: $!";
+    while (<$din>) {
+        chomp;
+        push @$words_array, lc $_;
+        $words_hash->{$_} = 1;
+    }
+    close $din or die $!;
+}
